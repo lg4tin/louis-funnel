2440757061037b8a8471a64af395ea95cf342f1d
lg4tin <97006319+lg4tin@users.noreply.github.com>
2026-01-23 22:39:16 -0500

    loom
    loome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -155,13 +155,13 @@ export default function Home() {
                 Goal: explain a complex workflow in under 30 seconds with UI clarity and a strong hook.
               </p>
               <div className="embed">
-              <iframe
-                src={"https://www.youtube.com/embed/=QUanGuJMJys&feature=youtu.be"}
-                title="Case Study 1"
-                allow="autoplay; encrypted-media; picture-in-picture"
-                allowFullScreen
-              />
-            </div>
+                <iframe
+                  src={"https://www.youtube.com/embed/=QUanGuJMJys"}
+                  title="Case Study 1"
+                  allow="autoplay; encrypted-media; picture-in-picture"
+                  allowFullScreen
+                />
+              </div>
 
               <ul>
                 <li>Immediate product understanding</li>
